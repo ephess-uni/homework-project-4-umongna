92a4bb78c15aff827899054f658bd5e9c401853f
umongna <165342875+umongna@users.noreply.github.com>
2024-04-26 15:07:41 -0500

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -36,7 +36,7 @@ def add_date_range(values, start_date):
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
     date_list = date_range(start_date, len(values))
-    res = list(zip(date_list, values)
+    res = list(zip(date_list, values))
     return res
 
 
